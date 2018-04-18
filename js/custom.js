468d1d60cc11ec2330a694495b1baab1efa563b5
Aneejian Home <kannansuresh@outlook.com>
2018-04-18 23:14:54 +0530

    js

--- js/custom.js
+++ js/custom.js
@@ -3,6 +3,10 @@ $(window).load(function(){
     $('.preloader').fadeOut(1000); // set duration in brackets    
 });
 
+$(#'blog').onclick = function () {
+    location.href = "https://www.aneejian.com";
+};
+
 $(function() {
     new WOW().init();
     $('.templatemo-nav').singlePageNav({

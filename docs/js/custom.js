b537a80ccb41e3c5d694ff368daab6ad8d454e52
Aneejian <aneejian@gmail.com>
2018-04-24 13:47:45 +0530

    video

--- docs/js/custom.js
+++ docs/js/custom.js
@@ -1,27 +1,28 @@
 // preloader
-// $(window).load(function(){
-//     $('.preloader').fadeOut(1000); // set duration in brackets    
-// });
+$(window).load(function(){
+    // $('.preloader').fadeOut(1000); // set duration in brackets 
+    testConnection();
+});
 
 $('#blog').onclick = function () {
     location.href = "https://www.aneejian.com";
-};  
+};
 
-$(function() {
+$(function () {
     new WOW().init();
     $('.templatemo-nav').singlePageNav({
-    	offset: 70
+        offset: 70
     });
 
     /* Hide mobile menu after clicking on a link
     -----------------------------------------------*/
-    $('.navbar-collapse a').click(function(){
+    $('.navbar-collapse a').click(function () {
         $(".navbar-collapse").collapse('hide');
     });
 })
 
 function changePage(event) {
-    if($(event.target).hasClass('external')) {
+    if ($(event.target).hasClass('external')) {
         window.location.href = $(event.target).attr('href');
         return;
     }
@@ -29,4 +30,24 @@ function changePage(event) {
 }
 $(function () {
     $('.nav li').click(changePage);
-})  
+})
+
+function testConnection() {
+    var playerFrame = document.getElementById('ytplayer');
+    var ySource = 'https://www.youtube.com/embed/KmaOW4PhuII';
+    var vSource = 'https://player.vimeo.com/video/265899201';
+    try {
+        var image = new Image();
+        image.onload = function () {
+            playerFrame.setAttribute('src', ySource);
+            alert('check done');
+        };
+        image.onerror = function () {
+            playerFrame.setAttribute('src', vSource);
+            alert('check done');
+        };
+        image.src = "http://youtube.com/favicon.ico";
+    } catch (error) {
+        playerFrame.setAttribute('src', source);
+    }
+}

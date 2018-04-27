814c88a2871fc10d84e970274a931e6e9686d867
Aneejian Home <kannansuresh@outlook.com>
2018-04-28 02:19:31 +0530

    style

--- docs/js/downloadstats.js
+++ docs/js/downloadstats.js
@@ -1,3 +1,4 @@
+var apiRoot = "https://api.github.com/";
 var latestReleaseUrl = "https://api.github.com/repos/aneejian/Change-Case-Excel-Add-In/releases/latest";
 var releaseUrl = 'https://github.com/aneejian/Change-Case-Excel-Add-In/releases/download/v3.0/';
 var fileName = 'change_case_excel_addin';
@@ -10,46 +11,90 @@ var fileTypes = {
 var totalDownloadCount = 0;
 var exeData = {
     url: fileTypes.exe,
-    downloads: 0,
+    downloads: 50,
     size: '961 KB',
     name: fileName + '.exe'
 }
 var zipData = {
     url: fileTypes.zip,
-    downloads: 0,
+    downloads: 50,
     size: '905 KB',
     name: fileName + '.zip'
 }
 var rarData = {
     url: fileTypes.rar,
-    downloads: 0,
+    downloads: 50,
     size: '853 KB',
     name: fileName + '.rar'
 }
 
-setAssetData();
+// setAssetData();
 
-var settings = {
-    "async": true,
-    "crossDomain": true,
-    "url": "https://api.github.com/repos/aneejian/Change-Case-Excel-Add-In/releases/latest",
-    "method": "GET",
-    'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'
-};
-
-$.ajax(settings).done(function (response) {
-    var assets = response.assets;
-    assets.forEach(getAssetData);
-    setAssetData();
+$(window).ready(function () {
+    getStats();    
 });
 
 
+
+// var settings = {
+//     "async": true,
+//     "crossDomain": true,
+//     "url": "https://api.github.com/repos/aneejian/Change-Case-Excel-Add-In/releases/latest",
+//     "method": "GET",
+// };
+
+// $.ajax(settings).done(function (response) {
+//     var assets = response.assets;
+//     assets.forEach(getAssetData);
+//     setAssetData();
+// });
+
+
+
+
+// $('#exe').click(function (e) {
+//     e.preventDefault();
+//     window.location.href = exeData.url;
+// });
+
+// $('#zip').click(function (e) {
+//     e.preventDefault();
+//     window.location.href = zipData.url;
+// });
+
+// $('#rar').click(function (e) {
+//     e.preventDefault();
+//     window.location.href = rarData.url;
+// });
+
+
+// Callback function for getting release stats
+function getStats() {
+    console.log('Fetching stats');
+    var user = 'aneejian';
+    var repository = 'Change-Case-Excel-Add-In';
+    var url = apiRoot + "repos/" + user + "/" + repository + "/releases/latest";
+    $.getJSON(url, setStats).fail(setDownloadLinksAndInfo);
+}
+
+function setStats(data){
+    console.log(data);
+    var releaseAssets = data.assets;
+    console.log(releaseAssets);
+    $.each(releaseAssets, function (index, item) {
+        console.log(item);
+        getAssetData(item);
+    });
+    setDownloadLinksAndInfo();
+}
+
 function getAssetData(asset) {
     totalDownloadCount += asset.download_count;
+    console.log(totalDownloadCount);
     var assetData = {
         url: asset.browser_download_url,
         downloads: asset.downloads,
-        size: Math.round(asset.size / 1024) + ' KB',    
+        size: Math.round(asset.size / 1024) + ' KB',
         name: asset.name
     }
     var extension = assetData.name.split('.').pop();
@@ -66,26 +111,12 @@ function getAssetData(asset) {
         default:
             break;
     }
-    $('#totalDownloads').text(totalDownloadCount);    
 }
 
-function setAssetData() {
+function setDownloadLinksAndInfo() {
+    console.log(totalDownloadCount);
+    $('#totalDownloads').text(totalDownloadCount);
     $('#exe').attr('onclick', "window.location.href='" + exeData.url + "'");
     $('#zip').attr('onclick', "window.location.href='" + zipData.url + "'");
     $('#rar').attr('onclick', "window.location.href='" + rarData.url + "'");
 }
-
-// $('#exe').click(function (e) {
-//     e.preventDefault();
-//     window.location.href = exeData.url;
-// });
-
-// $('#zip').click(function (e) {
-//     e.preventDefault();
-//     window.location.href = zipData.url;
-// });
-
-// $('#rar').click(function (e) {
-//     e.preventDefault();
-//     window.location.href = rarData.url;
-// });
